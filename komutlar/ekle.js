1d742c9ae649a0b4a0832dfef11e106a3cd7dfb6
desupretty <41763031+desupretty@users.noreply.github.com>
2020-08-04 17:06:51 +0300

    Update ekle.js

--- komutlar/ekle.js
+++ komutlar/ekle.js
@@ -5,7 +5,7 @@ exports.run = async(client, message, args) => {
   let kisi = message.mentions.members.first()
   if(!kisi) return message.channel.send('Birisini etiketlemelisin.')
   if(!args[1]) return message.channel.send('Paylaşım kategorisi girmen gerekiyor.')
-   if(!['wallpaper','emoji','istek','gif','pp'].includes(args[1])) return message.channel.send('Kategorileri doğru girmelisin.')
+   if(!['gif','pp'].includes(args[1])) return message.channel.send('Kategorileri doğru girmelisin.')
    if(!args[2]) return message.channel.send('Sayı girmelisin.')
   if(isNaN(args[2]))return message.channel.send('Sayı girmelisin.')
   if(args[2].startsWith('-')) return message.channel.send('Ekleme yapıyorsun çıkarma değil.')
